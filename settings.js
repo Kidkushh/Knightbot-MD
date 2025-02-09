1e15efd3ffa691317a37c9647d49e2a29d8b44d3
Kidkushh <khatinahama@gmail.com>
2025-02-09 05:29:28 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Knightbot-MD",
+  botOwner: 'Kidkushh', // Your name
+  ownerNumber: '263784933028, //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
